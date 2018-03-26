42cd032f466c06b5999dafe4dc98eb965226e162
Bilal H <hussain.bilal000786@gmail.com>
2018-03-27 00:44:51 +0530

    Added Spotlight and Artists UI

--- screens/Artists.js
+++ screens/Artists.js
@@ -1,19 +1,204 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Image, Button } from 'react-native';
+
+import { LinearGradient} from 'expo';
+
+import { TopTitle, TitleText } from '../components/header';
+import Button_bh from '../components/body/Button';
+import styles from '../style';
 
 class Artists extends Component {
   render() {
     return (
-      <View>
-        <Text>Artists</Text>
-        <Text>Artists</Text>
-        <Text>Artists</Text>
-        <Text>Artists</Text>
-        <Text>Artists</Text>
-        <Text>Artists</Text>
-      </View>
+      <ScrollView style={[styles.profile, {flex: 1}]}>
+          <TopTitle text={'ARTISTS'} />
+          <TitleText text={'ARTISTS'} />
+          <Image
+            style={styles.carousel}
+            source={require('../img/Image_art2.png')} />
+
+            <View style={localStyles.descArtist}>
+                <Text style={localStyles.artistTitle}>JUDITH HILL</Text>
+                <Text style={localStyles.artistAbout}>Mauris commodo interdum lacus ut scelerisque. Nam eleifend enim quis cursus sodales. Morbi vehicula tristique tristique. Aliquam erat volutpat. Quisque posuere in metus ac vehicula. Maecenas maximus, orci et hendrerit pharetra, orci arcu rhoncus sem, et dignissim nisl urna ac augue. Suspendisse velit est, vulputate eget hendrerit at, efficitur eget sapien. Praesent facilisis, metus id volutpat rutrum, ipsum diam scelerisque tortor, at volutpat nisi massa non magna. Pellentesque venenatis lobortis feugiat. Vivamus luctus lobortis arcu nec ullamcorper. In eu varius mi, vel sodales lacus. Quisque lobortis non magna a tempus.</Text>
+            </View>
+
+            <View style={localStyles.videoBar}>
+              <Image
+                style={localStyles.videoMain}
+                source={require('../img/Video_Main.png')} />
+
+              <View style={localStyles.sideVideos}>
+                <Image source={require('../img/Video_1.png')} />
+                <Image source={require('../img/Video_2.png')} />
+                <Image source={require('../img/Video_3.png')} />
+              </View>
+            </View>
+            <Text style={localStyles.heading}>ALBUMS</Text>
+          <View style={styles.mainAlbumSection}>
+            <View style={localStyles.AlbumsSec}>
+              <View style={localStyles.album}>
+
+                <Image style={{ width: '20%', marginLeft: 5 }} source={require('../img/Image_video1.png')} />
+                <View style={localStyles.albumVertical}>
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>AS TRAINS BY</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>TURN UP</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>ANGEL IN THE DARK</Text>
+                  </View>
+
+                </View>
+
+                <LinearGradient
+                  colors={['#000', '#636363']}
+                  start={[1, 1]}
+                  end={[1, 0]}
+                  style={localStyles.gradientStyle}>
+                  <Button_bh title="REGISTER" onPress={() => alert('REGISTER')} width="80"/>
+               </LinearGradient>
+
+              </View>
+            </View>
+            <View style={localStyles.AlbumsSec}>
+              <View style={localStyles.album}>
+
+                <Image style={{ width: '20%', marginLeft: 5 }} source={require('../img/Image_video1.png')} />
+                <View style={localStyles.albumVertical}>
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>AS TRAINS BY</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>TURN UP</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>ANGEL IN THE DARK</Text>
+                  </View>
+
+                </View>
+
+                <LinearGradient
+                  colors={['#000', '#636363']}
+                  start={[1, 1]}
+                  end={[1, 0]}
+                  style={localStyles.gradientStyle}>
+                  <Button_bh title="REGISTER" onPress={() => alert('REGISTER')} width="80"/>
+               </LinearGradient>
+
+              </View>
+            </View>
+            <View style={localStyles.AlbumsSec}>
+              <View style={localStyles.album}>
+
+                <Image style={{ width: '20%', marginLeft: 5 }} source={require('../img/Image_video1.png')} />
+                <View style={localStyles.albumVertical}>
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>AS TRAINS BY</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>TURN UP</Text>
+                  </View>
+
+                  <View style={localStyles.albumSongs}>
+                    <Text style={localStyles.songTitle}>ANGEL IN THE DARK</Text>
+                  </View>
+
+                </View>
+
+                <LinearGradient
+                  colors={['#000', '#636363']}
+                  start={[1, 1]}
+                  end={[1, 0]}
+                  style={localStyles.gradientStyle}>
+                  <Button_bh title="REGISTER" onPress={() => alert('REGISTER')} width="80"/>
+               </LinearGradient>
+
+              </View>
+            </View>
+          </View>
+      </ScrollView>
     )
   }
+}
+
+const localStyles = {
+  descArtist: {
+    margin: 10,
+    flexDirection: 'column'
+  },
+  artistTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  artistAbout: {
+    color: 'white',
+    fontSize: 10
+  },
+  videoBar: {
+    flex: 1,
+    flexDirection: 'row',
+    marginVertical: 5,
+    marginHorizontal: 10
+  },
+  videoMain: {
+    width: '75%',
+    marginHorizontal: 5
+  },
+  sideVideos: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  heading: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+    margin: 15
+  },
+  album: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+
+  },
+  albumVertical: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginHorizontal: 10,
+    width: '50%'
+  },
+  albumSongs: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  songTitle: {
+    color: 'white',
+    fontSize: 10,
+    marginHorizontal: 10
+  },
+  gradientStyle: {
+    height: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    right: 0
+  },
+  mainAlbumSection: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  AlbumsSec: {
+    marginVertical: 5
+  }
+
 }
 
 export default Artists;
--- screens/Spotlight.js
+++ screens/Spotlight.js
@@ -1,19 +1,178 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
+import { LinearGradient} from 'expo';
+
+import { TopTitle, TitleText } from '../components/header';
+import styles from '../style';
 
 class Spotlight extends Component {
   render() {
     return (
-      <View>
-        <Text>Spotlight</Text>
-        <Text>Spotlight</Text>
-        <Text>Spotlight</Text>
-        <Text>Spotlight</Text>
-        <Text>Spotlight</Text>
-        <Text>Spotlight</Text>
-      </View>
+      <ScrollView style={[styles.profile, {flex: 1}]}>
+          <TopTitle text={'ARTISTS'} />
+          <TitleText text={'SPOTLIGHT'} />
+          <Image
+            style={styles.carousel}
+            source={require('../img/Carousel.png')} />
+
+            <ScrollView style={{flex:1}} horizontal={true}>
+              <View style={localStyles.ticketPortal}>
+                <Text style={localStyles.ticketText}>AUGUST 10-26, 2017</Text>
+                <View style={localStyles.artistCont}>
+                  <Text style={localStyles.artistText}>Corinne Bailey Rae</Text>
+                  <Text style={localStyles.artistDesc}>HOLLYWOOD BOWL</Text>
+                </View>
+              </View>
+              <View style={localStyles.ticketPortal}>
+                <Text style={localStyles.ticketText}>SEPTEMBER 2, 2017</Text>
+                <View style={localStyles.artistCont}>
+                  <Text style={localStyles.artistText}>James Morrison</Text>
+                  <Text style={localStyles.artistDesc}>EL REY THEATRE</Text>
+                </View>
+              </View>
+              <View style={localStyles.ticketPortal}>
+                <Text style={localStyles.ticketText}>AUGUST 10-26, 2017</Text>
+                <View style={localStyles.artistCont}>
+                  <Text style={localStyles.artistText}>Corinne Bailey Rae</Text>
+                  <Text style={localStyles.artistDesc}>HOLLYWOOD BOWL</Text>
+                </View>
+              </View>
+              <View style={localStyles.ticketPortal}>
+                <Text style={localStyles.ticketText}>AUGUST 10-26, 2017</Text>
+                <View style={localStyles.artistCont}>
+                  <Text style={localStyles.artistText}>Corinne Bailey Rae</Text>
+                  <Text style={localStyles.artistDesc}>HOLLYWOOD BOWL</Text>
+                </View>
+              </View>
+            </ScrollView>
+
+            <View style={{flex:1}}>
+
+              <View style={localStyles.trendView}>
+                <Image
+                  style={localStyles.trendImg}
+                  source={require('../img/Image_trending1.png')}/>
+
+                  <LinearGradient
+                    colors={['#da2df9', '#000']}
+                    start={[1, 1]}
+                    end={[1, 0]}
+                    style={localStyles.gradientStyle}>
+                  <View style={localStyles.trendCont}>
+                    <Text style={localStyles.trendHead}>T R E N D I N G</Text>
+                    <View style={localStyles.trendAlbum}>
+                        <Text style={localStyles.artistText}>Janelle Monae</Text>
+                        <Text style={localStyles.albumSong}>MAKE ME FEEL</Text>
+                    </View>
+                  </View>
+                  </LinearGradient>
+
+              </View>
+              <View style={localStyles.trendView}>
+                <Image
+                  style={localStyles.trendImg}
+                  source={require('../img/Image_trending2.png')}/>
+                  <LinearGradient
+                    colors={['#da2df9', '#000']}
+                    start={[1, 1]}
+                    end={[1, 0]}
+                    style={localStyles.gradientStyle}>
+                  <View style={localStyles.trendCont}>
+                    <Text style={localStyles.trendHead}>T R E N D I N G</Text>
+                    <View style={localStyles.trendAlbum}>
+                      <Text style={localStyles.artistText}>5 Seconds of Summer</Text>
+                      <Text style={localStyles.albumSong}>WANT YOU BACK</Text>
+                    </View>
+                  </View>
+                  </LinearGradient>
+
+              </View>
+
+              <View style={localStyles.trendView}>
+                <Image
+                  style={localStyles.trendImg}
+                  source={require('../img/Image_trending1.png')}/>
+
+                  <LinearGradient
+                    colors={['#da2df9', '#000']}
+                    start={[1, 1]}
+                    end={[1, 0]}
+                    style={localStyles.gradientStyle}>
+                  <View style={localStyles.trendCont}>
+                    <Text style={localStyles.trendHead}>T R E N D I N G</Text>
+                    <View style={localStyles.trendAlbum}>
+                      <Text style={localStyles.artistText}>Janelle Monae</Text>
+                      <Text style={localStyles.albumSong}>MAKE ME FEEL</Text>
+                    </View>
+                    </View>
+                    </LinearGradient>
+
+                  </View>
+
+            </View>
+
+      </ScrollView>
     )
   }
+}
+
+localStyles = {
+  gradientStyle: {
+    width: 250
+  },
+  ticketPortal: {
+    backgroundColor: '#da2df9',
+    width: 150,
+    height: 100,
+    marginHorizontal: 2,
+    marginVertical: 2
+  },
+  ticketText: {
+    color: 'white',
+    fontSize: 10,
+    fontWeight: '500',
+    margin: 7
+  },
+  artistCont: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    margin: 7
+  },
+  artistText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '800',
+
+  },
+  artistDesc: {
+    color: 'white',
+    fontSize: 12,
+  },
+  trendView: {
+    backgroundColor: '#da2df9',
+    flexDirection: 'row',
+    marginVertical: 5
+  },
+  trendCont: {
+    flexDirection: 'column',
+    marginHorizontal: 5,
+    marginVertical: 8
+  },
+  trendHead: {
+    color: 'white',
+    fontSize: 12,
+    opacity: 0.5,
+    marginVertical: 5
+  },
+  trendImg: {
+    width: '40%'
+  },
+  albumSong: {
+    color: 'white',
+    fontSize: 10
+  }
+
 }
 
 export default Spotlight;
